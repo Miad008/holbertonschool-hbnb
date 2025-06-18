32bdfd9d0d99b34d0368b7911cd7897765e3ea71
Batoul-Alsaeed <BatoulAlsaeed@hotmail.com>
2025-06-18 17:54:08 +0300

    Add Place model with attributes and relationships to amenities and reviews

---
+++ part2/app/core/models/place.py
@@ -0,0 +1,23 @@
+from app.core.models.base import BaseModel
+
+class Place(BaseModel):
+    def __init__(self, name, description, address, owner_id, id=None, created_at=None, updated_at=None):
+        super().__init__(id=id, created_at=created_at, updated_at=updated_at)
+        self.name = name
+        self.description = description
+        self.address = address
+        self.owner_id = owner_id  # ID خاص بالمستخدم الذي يملك هذا المكان
+        self.amenity_ids = []     # قائمة IDs للأشياء المرتبطة
+        self.review_ids = []      # قائمة IDs للتقييمات المرتبطة
+
+    def to_dict(self):
+        base_dict = super().to_dict()
+        base_dict.update({
+            "name": self.name,
+            "description": self.description,
+            "address": self.address,
+            "owner_id": self.owner_id,
+            "amenity_ids": self.amenity_ids,
+            "review_ids": self.review_ids
+        })
+        return base_dict
