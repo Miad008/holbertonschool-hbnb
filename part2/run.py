ab6174d902b3a47a2c34f6e09d234703913aed71
Batoul-Alsaeed <BatoulAlsaeed@hotmail.com>
2025-06-18 14:12:00 +0300

    Add user API endpoints for create, retrieve, list, and delete operations

--- part2/run.py
+++ part2/run.py
@@ -8,6 +8,8 @@ api = Api(app)
 # نستورد الـ Namespace الجاهز (مثل /ping)
 from app.api.example import ns as example_ns
 api.add_namespace(example_ns)
+from app.api.users import ns as users_ns
+api.add_namespace(users_ns)
 
 if __name__ == "__main__":
     app.run(debug=True)
