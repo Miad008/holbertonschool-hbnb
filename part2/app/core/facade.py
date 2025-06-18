5e13ca8f081112569b328f7ad03fc6162902f048
Batoul-Alsaeed <BatoulAlsaeed@hotmail.com>
2025-06-18 14:03:50 +0300

    Add HBnBFacade class to manage business logic and connect with in-memory storage

--- part2/app/core/facade.py
+++ part2/app/core/facade.py
@@ -1,1 +1,23 @@
+from app.repository.in_memory_storage import InMemoryStorage
 
+class HBnBFacade:
+    def __init__(self):
+        self.storage = InMemoryStorage()
+
+    def create_user(self, user_data):
+        """ينشئ مستخدم جديد ويخزّنه"""
+        from app.core.models.user import User
+        new_user = User(**user_data)
+        return self.storage.add("users", new_user)
+
+    def get_user(self, user_id):
+        """يرجع مستخدم حسب ID"""
+        return self.storage.get("users", user_id)
+
+    def get_all_users(self):
+        """يرجع كل المستخدمين"""
+        return self.storage.get_all("users")
+
+    def delete_user(self, user_id):
+        """يحذف مستخدم حسب ID"""
+        return self.storage.delete("users", user_id)
