c50566b01f3d26b1a989da2d7cb8cce0ad0a604d
Batoul-Alsaeed <BatoulAlsaeed@hotmail.com>
2025-06-18 17:39:26 +0300

    Update run.py

--- part2/run.py
+++ part2/run.py
@@ -5,14 +5,14 @@ from flask_restx import Api
 app = Flask(__name__)
 api = Api(app)
 
-# نستورد الـ Namespace الجاهز (مثل /ping)
+# استيراد ping
 from app.api.example import ns as example_ns
 api.add_namespace(example_ns)
 
-# استيراد Namespace الخاص بالمستخدمين (/users)
+# استيراد users
 from app.api.users import ns as users_ns
 api.add_namespace(users_ns)
 
-# تشغيل السيرفر
+# تشغيل التطبيق
 if __name__ == "__main__":
     app.run(debug=True)
