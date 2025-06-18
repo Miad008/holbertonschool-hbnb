1a223406d2193634429534ba400f3f5983efc132
Batoul-Alsaeed <BatoulAlsaeed@hotmail.com>
2025-06-18 14:20:46 +0300

    Register user API namespace in run.py

--- part2/run.py
+++ part2/run.py
@@ -8,8 +8,11 @@ api = Api(app)
 # نستورد الـ Namespace الجاهز (مثل /ping)
 from app.api.example import ns as example_ns
 api.add_namespace(example_ns)
+
+# استيراد Namespace الخاص بالمستخدمين (/users)
 from app.api.users import ns as users_ns
 api.add_namespace(users_ns)
 
+# تشغيل السيرفر
 if __name__ == "__main__":
     app.run(debug=True)
