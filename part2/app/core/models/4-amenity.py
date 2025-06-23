fbeb98d5aba3561f155d2cadcd35259023455ad8
Miad008 <10694@holbertonstudents.com>
2025-06-23 09:09:15 +0000

    Add Amenity model class with name and description attributes

--- part2/app/core/models/4-amenity.py
+++ part2/app/core/models/4-amenity.py
@@ -7,14 +7,16 @@ class Amenity(BaseModel):
     Attributes:
         name (str): Name of the amenity (e.g., WiFi, Pool).
     """
-    
+
     def __init__(self, name, id=None, created_at=None, updated_at=None):
         super().__init__(id=id, created_at=created_at, updated_at=updated_at)
         self.name = name  # اسم الميزة مثل "WiFi", "Kitchen", إلخ
+	self.description = description
 
     def to_dict(self):
         base_dict = super().to_dict()
         base_dict.update({
-            "name": self.name
+	"name": self.name,
+	"description": self.description
         })
         return base_dict
