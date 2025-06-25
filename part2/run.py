95775117cfcc6e539c5daee59492abbcc03acc8a
Batoul-Alsaeed <BatoulAlsaeed@hotmail.com>
2025-06-25 11:00:19 +0300

    Register Place namespace in run.py under /api/v1/places route

--- part2/run.py
+++ part2/run.py
@@ -13,11 +13,18 @@ api.add_namespace(example_ns)
 from app.api.users import ns as users_ns
 api.add_namespace(users_ns)
 
+# استيراد amenities
 from app.api.amenities import ns as amenity_ns
 api.add_namespace(amenity_ns)
+
+# استيراد places
+from app.api.places import ns as places_ns
+api.add_namespace(places_ns) #(places_ns, path='/api/v1/places')
+
+# استيراد reviews
+from app.api.reviews import ns as reviews_ns
+api.add_namespace(reviews_ns)
 
 # تشغيل التطبيق
 if __name__ == "__main__":
     app.run(debug=True)
-from app.api.reviews import ns as reviews_ns
-api.add_namespace(reviews_ns)
