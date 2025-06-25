6b789586c6a9c31a393a3df5635205f015f741b7
Miad008 <10694@holbertonstudents.com>
2025-06-25 14:25:41 +0300

    Update run.py

--- part2/run.py
+++ part2/run.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_restx import Api
+from config import config
 
 # إنشاء التطبيق
 app = Flask(__name__)
+app.config.from_object(config['default'])
 api = Api(app)
 
 # استيراد ping
