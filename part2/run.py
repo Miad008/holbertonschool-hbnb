4c26a728815f30e202d0fb88189aeb2a37df32ea
Batoul-Alsaeed <BatoulAlsaeed@hotmail.com>
2025-06-25 15:51:43 +0300

    Update run.py

--- part2/run.py
+++ part2/run.py
@@ -12,19 +12,19 @@ from app.api.example import ns as example_ns
 api.add_namespace(example_ns)
 
 # استيراد users
-from app.api.users import ns as users_ns
+from app.api.v1.users import ns as users_ns
 api.add_namespace(users_ns, path='/api/v1/users')
 
 # استيراد amenities
-from app.api.amenities import ns as amenity_ns
+from app.api.v1.amenities import ns as amenity_ns
 api.add_namespace(amenity_ns, path='/api/v1/amenities')
 
 # استيراد places
-from app.api.places import ns as places_ns
+from app.api.v1.places import ns as places_ns
 api.add_namespace(places_ns, path='/api/v1/places')
 
 # استيراد reviews
-from app.api.reviews import ns as reviews_ns
+from app.api.v1.reviews import ns as reviews_ns
 api.add_namespace(reviews_ns, path='/api/v1/reviews')
 
 # تشغيل التطبيق
