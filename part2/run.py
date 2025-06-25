93abb45c90a917f73e055d790f3e0bcc825e9b8c
Batoul-Alsaeed <BatoulAlsaeed@hotmail.com>
2025-06-25 12:02:11 +0300

    Update run.py

--- part2/run.py
+++ part2/run.py
@@ -11,19 +11,19 @@ api.add_namespace(example_ns)
 
 # استيراد users
 from app.api.users import ns as users_ns
-api.add_namespace(users_ns)
+api.add_namespace(users_ns, path='/api/v1/users')
 
 # استيراد amenities
 from app.api.amenities import ns as amenity_ns
-api.add_namespace(amenity_ns)
+api.add_namespace(amenity_ns, path='/api/v1/amenities')
 
 # استيراد places
 from app.api.places import ns as places_ns
-api.add_namespace(places_ns) #(places_ns, path='/api/v1/places')
+api.add_namespace(places_ns, path='/api/v1/places')
 
 # استيراد reviews
 from app.api.reviews import ns as reviews_ns
-api.add_namespace(reviews_ns)
+api.add_namespace(reviews_ns, path='/api/v1/reviews')
 
 # تشغيل التطبيق
 if __name__ == "__main__":
