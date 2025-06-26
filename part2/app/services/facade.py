1abc361b5e5497e091b39dcc087b054ee3b9d240
Batoul-Alsaeed <BatoulAlsaeed@hotmail.com>
2025-06-26 11:22:29 +0300

    fix

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -2,7 +2,10 @@ from app.persistence.repository import InMemoryRepository
 
 class HBnBFacade:
     def __init__(self):
-        self.storage = InMemoryStorage()
+        self.user_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
 
     # ----- User Methods -----
     def create_user(self, user_data):
