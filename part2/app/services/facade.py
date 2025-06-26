1632a91ae88a615750bb7cb33609107d8fa913a4
Batoul-Alsaeed <BatoulAlsaeed@hotmail.com>
2025-06-26 12:42:48 +0300

    refactor: split repositories per entity and add get_user_by_email to facade

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -11,16 +11,19 @@ class HBnBFacade:
     def create_user(self, user_data):
         from app.models.user import User  
         new_user = User(**user_data)
-        return self.storage.add("users", new_user)
+        return self.user_repo.add(new_user)
 
     def get_user(self, user_id):
-        return self.storage.get("users", user_id)
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
 
     def get_all_users(self):
-        return self.storage.get_all("users")
+        return self.user_repo.get_all()
 
     def update_user(self, user_id, user_data):
-        user = self.storage.get("users", user_id)
+        user = self.user_repo.get(user_id)
         if not user:
             return None
         for key, value in user_data.items():
@@ -30,21 +33,21 @@ class HBnBFacade:
         return user
 
     def delete_user(self, user_id):
-        return self.storage.delete("users", user_id)
+        return self.user_repo.delete(user_id)
 
     # ----- Amenity Methods -----
     def create_amenity(self, amenity_data):
         from app.models.amenity import Amenity
-        if not all(field in amenity_data and amenity_data[field] for field in ["name"]):
+        if not amenity_data.get("name"):
             raise ValueError("Missing required amenity fields")
         new_amenity = Amenity(**amenity_data)
-        return self.storage.add("amenities", new_amenity)
+        return self.amenity_repo.add(new_amenity)
 
     def get_amenity(self, amenity_id):
-        return self.storage.get("amenities", amenity_id)
+        return self.amenity_repo.get(amenity_id)
 
     def get_all_amenities(self):
-        return self.storage.get_all("amenities")
+        return self.amenity_repo.get_all()
 
     def update_amenity(self, amenity_id, updated_data):
         amenity = self.get_amenity(amenity_id)
@@ -56,19 +59,18 @@ class HBnBFacade:
 
     # ----- Place Methods -----
     def create_place(self, place_data):
+        from app.models.place import Place
         required_fields = ["name", "address", "owner_id", "price"]
         if not all(field in place_data and place_data[field] for field in required_fields):
             raise ValueError("Missing required place fields")
-        
-        from app.models.place import Place
         place = Place(**place_data)
-        return self.storage.add("places", place)
+        return self.place_repo.add(place)
 
     def get_place(self, place_id):
-        return self.storage.get("places", place_id)
+        return self.place_repo.get(place_id)
 
     def get_all_places(self):
-        return self.storage.get_all("places")
+        return self.place_repo.get_all()
 
     def update_place(self, place_id, updated_data):
         place = self.get_place(place_id)
@@ -82,19 +84,18 @@ class HBnBFacade:
 
     # ----- Review Methods -----
     def create_review(self, review_data):
+        from app.models.review import Review
         required_fields = ["user_id", "place_id", "text"]
         if not all(field in review_data and review_data[field] for field in required_fields):
             raise ValueError("Missing required review fields")
-
-        from app.models.review import Review
         review = Review(**review_data)
-        return self.storage.add("reviews", review)
+        return self.review_repo.add(review)
 
     def get_review(self, review_id):
-        return self.storage.get("reviews", review_id)
+        return self.review_repo.get(review_id)
 
     def get_all_reviews(self):
-        return self.storage.get_all("reviews")
+        return self.review_repo.get_all()
 
     def update_review(self, review_id, updated_data):
         review = self.get_review(review_id)
@@ -110,9 +111,7 @@ class HBnBFacade:
         review = self.get_review(review_id)
         if not review:
             return None
-
         place = self.get_place(review.place_id)
         if place and review_id in place.review_ids:
             place.review_ids.remove(review_id)
-
-        return self.storage.delete("reviews", review_id)
+        return self.review_repo.delete(review_id)
