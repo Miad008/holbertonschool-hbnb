95a5c68fba1d8df497fb1f2bccd20d75f17de39a
Batoul-Alsaeed <BatoulAlsaeed@hotmail.com>
2025-06-26 11:06:32 +0300

    Delete part2/app/api/v1/example.py

--- part2/app/api/v1/example.py
+++
@@ -1,8 +0,0 @@
-from flask_restx import Namespace, Resource
-
-ns = Namespace('ping', description='Ping test namespace')
-
-@ns.route('/')
-class PingResource(Resource):
-    def get(self):
-        return {"message": "pong!"}
