90af65c08c5f4cee53ab9ac13e545560466ef2ce
Batoul-Alsaeed <BatoulAlsaeed@hotmail.com>
2025-06-26 11:14:29 +0300

    Update repository.py

--- part2/app/persistence/repository.py
+++ part2/app/persistence/repository.py
@@ -1,34 +1,52 @@
-import uuid
+from abc import ABC, abstractmethod
 
-class InMemoryStorage:
+class Repository(ABC):
+    @abstractmethod
+    def add(self, obj):
+        pass
+
+    @abstractmethod
+    def get(self, obj_id):
+        pass
+
+    @abstractmethod
+    def get_all(self):
+        pass
+
+    @abstractmethod
+    def update(self, obj_id, data):
+        pass
+
+    @abstractmethod
+    def delete(self, obj_id):
+        pass
+
+    @abstractmethod
+    def get_by_attribute(self, attr_name, attr_value):
+        pass
+
+
+class InMemoryRepository(Repository):
     def __init__(self):
-        # تخزين كل نوع من البيانات في قاموس خاص فيه
-        self.data = {
-            "users": {},
-            "places": {},
-            "reviews": {},
-            "amenities": {}
-        }
+        self._storage = {}
 
-    def generate_id(self):
-        """ينتج ID فريد باستخدام uuid4"""
-        return str(uuid.uuid4())
+    def add(self, obj):
+        self._storage[obj.id] = obj
 
-    def add(self, entity_type, obj):
-        """يضيف كائن إلى التخزين"""
-        if not hasattr(obj, 'id'):
-            obj.id = self.generate_id()
-        self.data[entity_type][obj.id] = obj
-        return obj
+    def get(self, obj_id):
+        return self._storage.get(obj_id)
 
-    def get(self, entity_type, obj_id):
-        """يرجع كائن معين حسب النوع والـ ID"""
-        return self.data[entity_type].get(obj_id)
+    def get_all(self):
+        return list(self._storage.values())
 
-    def get_all(self, entity_type):
-        """يرجع جميع الكائنات من نوع معين"""
-        return list(self.data[entity_type].values())
+    def update(self, obj_id, data):
+        obj = self.get(obj_id)
+        if obj:
+            obj.update(data)
 
-    def delete(self, entity_type, obj_id):
-        """يحذف كائن حسب النوع والـ ID"""
-        return self.data[entity_type].pop(obj_id, None)
+    def delete(self, obj_id):
+        if obj_id in self._storage:
+            del self._storage[obj_id]
+
+    def get_by_attribute(self, attr_name, attr_value):
+        return next((obj for obj in self._storage.values() if getattr(obj, attr_name) == attr_value), None)
