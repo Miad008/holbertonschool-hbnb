bf655bfad3ae5d150fd1f9492ee2700c0ab5d8c2
Batoul-Alsaeed <BatoulAlsaeed@hotmail.com>
2025-06-30 21:06:57 +0300

    Initialize bcrypt for password hashing

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from app.config import DevelopmentConfig
 
+# Initialize extensions
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 def create_app(config_class=DevelopmentConfig):
+    """Application factory with config and extensions"""
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    # Initialize extensions
     db.init_app(app)
+    bcrypt.init_app(app)
 
     return app
