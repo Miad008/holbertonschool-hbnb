1b5b5762509a426d5ed5c767e3a1e9371c2236d8
rwwwn <ralbaraiki@gmail.com>
2025-07-08 10:04:13 +0300

    Update config.py

--- part3/app/config.py
+++ part3/app/config.py
@@ -1,11 +1,15 @@
 import os
 
 class Config:
-    """Base configuration"""
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    DEBUG = False
 
 class DevelopmentConfig(Config):
-    """Development config using SQLite"""
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///hbnb.db"
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
+}
