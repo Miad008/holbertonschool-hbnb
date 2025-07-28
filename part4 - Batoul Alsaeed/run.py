2c1b46b1401f750f32d4900d2bfc2089a70dc001
Batoul-Alsaeed <BatoulAlsaeed@hotmail.com>
2025-07-28 04:43:23 +0300

    Update run.py

--- part4 - Batoul Alsaeed/run.py
+++ part4 - Batoul Alsaeed/run.py
@@ -1,7 +1,11 @@
 from app import create_app
-from app.api.v1.users import ns as users_namespace, admin_ns as admin_users_namespace
-from app.api.v1.auth import auth as auth_namespace  # Task 2: JWT login namespace
 from flask_restx import Api
+from app.api.v1.users import ns as users_namespace, admin_ns as admin_users_namespace
+from app.api.v1.auth import auth as auth_namespace
+from app.api.v1.places import api as places_namespace
+from app.api.v1.reviews import ns as reviews_namespace
+from app.api.v1.auth import auth as auth_namespace
+from app.api.v1.amenities import ns as amenities_namespace
 
 app = create_app()
 api = Api(app, title="HBnB API", version="1.0", description="HBnB REST API")
@@ -9,7 +13,10 @@ api = Api(app, title="HBnB API", version="1.0", description="HBnB REST API")
 # Register all namespaces
 api.add_namespace(users_namespace, path="/api/v1/users")
 api.add_namespace(admin_users_namespace, path="/api/v1/admin_users")
+api.add_namespace(places_namespace, path="/api/v1/places")
+api.add_namespace(reviews_namespace, path="/api/v1/reviews")
 api.add_namespace(auth_namespace, path="/api/v1/auth")  # Task 2: Login route
+api.add_namespace(amenities_namespace, path="/api/v1/amenities")
 
 if __name__ == "__main__":
     app.run(debug=True)
